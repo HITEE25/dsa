83501870c165ae2a70b10434394c61178f4676e4
HITEE25 <hitee0025@gmail.com>
2025-06-23 19:31:31 +0530

    Update duque.cpp

--- duque.cpp
+++ duque.cpp
@@ -10,4 +10,19 @@ int main() {
     
     cout << d.front() << endl;
     cout << d.back() << endl;
+    
+    d.pop_front();
+    
+    cout << d.front() << endl;
+    cout << d.back() << endl;
+    
+    d.pop_back();
+    
+    if(d.empty()){
+        cout << "Queue is empty "<< endl;
+    }
+    else{
+        cout << "Queue is not empty "<< endl;
+    }
+    
 }
